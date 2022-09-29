ed588a019307d762d237e3710122112a3ab44a64
ThyDrSlen <drslen9@gmail.com>
2022-09-29 17:52:40 +0000

    function fiveToOneHundred done

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 //Code your solutions in this file
+function fiveToOneHundred(){
+    for(let i = 5;i <=100; i++){
+        console.log(i);
+    }
+};
+
+
+//Write a function fiveToOneHundred that console logs all numbers from 5 to 100.
+
+fiveToOneHundred();
