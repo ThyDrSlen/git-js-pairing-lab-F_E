47a1412cdb0aa160e0b1909f78cec84c7b56c848
ethaninniss <ethan.inniss@gmail.com>
2022-09-29 17:58:24 +0000

    function multiplesOfThree done

--- index.js
+++ index.js
@@ -9,3 +9,11 @@ function fiveToOneHundred(){
 //Write a function fiveToOneHundred that console logs all numbers from 5 to 100.
 
 fiveToOneHundred();
+
+function multiplesOfThree(){
+    for(let i = 3; i<=100; i= i + 3){
+        console.log(i);
+    }
+}
+
+multiplesOfThree();
